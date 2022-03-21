94a2bd82b07168b9af2a7b171d4dde8d910b1cf2
PatrykAdamski <patada7090@gmail.com>
2022-03-21 11:33:17 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HomeHeader from "./components/HomeHeader";
 import Navigation from "./components/Navigation";
 
 export default function App() {
   return (
     <div className="container-fluid gx-0">
       <Navigation></Navigation>
+      <HomeHeader></HomeHeader>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,11 @@ import { Link as LinkScroll } from "react-scroll";
 
 export default function Navigation() {
   return (
-    <div className="custom-navigation">
+    <div className="custom-navigation fixed-top">
       <nav className="container navbar py-0 navbar-expand-lg">
         <div className="display-6 ms-3 ms-md-0 custom-logo">BestShop</div>
         <button
-          className="navbar-toggler custom-btn py-4"
+          className="navbar-toggler custom-btn py-4 collapsed"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -15,12 +15,12 @@ export default function Navigation() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <div className="bg-dark custom-line1"></div>
-          <div className="bg-dark custom-line2"></div>
-          <div className="bg-dark custom-line3"></div>
+          <div className="custom-line1"></div>
+          <div className="custom-line2"></div>
+          <div className="custom-line3"></div>
         </button>
         <ul
-          className="collapse navbar-collapse navbar-nav text-uppercase justify-content-end"
+          className="collapse navbar-collapse navbar-nav text-uppercase justify-content-end bg-white"
           id="navbarSupportedContent"
         >
           <LinkScroll className="px-4 py-4 custom-nav-link">Why us</LinkScroll>
